16612419582189460362f8669a9f4317370d4c13
SeoHyun <79033073+se0hyun@users.noreply.github.com>
2023-07-18 16:29:12 +0900

    Update app.py
    웹앱 배포 위한 app.py 기본으로 수정

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+'''
 from flask import Flask
 from flask import request
 
@@ -18,3 +19,10 @@ out_ip = re.search(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', req.text)[1]
 @app.route("/")
 def home():
     return  '컴퓨터 이름 : {0}<br>내부 IP : {1}<br>외부 IP : {2}<br><br> 당신의 IP :{3}<br><br>Hello'.format(hostname, hostip, out_ip, request.remote_addr)
+'''
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, Flask!'
